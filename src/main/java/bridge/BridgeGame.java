32c4da4c1c9a1b4a544cd043237ea40034544b49
tngur1101 <tngur1101@naver.com>
2023-09-14 03:11:44 +0900

    feat: BridgeGame 게임 초기화 함수 생성
    게임 시작 전 세팅을 해주는 init 함수 생성

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,16 +1,44 @@
 package bridge;
 
+import Util.Validator;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
+
+    private static final String UP = "U";
+    private static final String DOWN = "D";
+    private static final String RESTART = "R";
+    private static final String QUIT = "Q";
+    private static final int MIN_BRIDGE_LENGTH = 3;
+    private static final int MAX_BRIDGE_LENGTH = 20;
+    private int TRY_NUMBER = 1;
+    private boolean GAME_RESULT = true;
+
+    private static BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+    private Bridge bridge;
+    private List<String> visited = new ArrayList<>();
+    private static Validator validator = new Validator();
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
+
+    public void init(String input){
+        validator.validateBridgeNumber(input, MIN_BRIDGE_LENGTH, MAX_BRIDGE_LENGTH);
+        int bridgeLength = Integer.parseInt(input);
+        List<String> bridgeList = bridgeMaker.makeBridge(bridgeLength);
+        bridge = new Bridge(bridgeList);
+    }
+
     public void move() {
+        
     }
 
     /**

62553fe4752b449751a34b874f6e2266ddb99c1d
tngur1101 <tngur1101@naver.com>
2023-09-14 02:53:10 +0900

    feat: BridgeMaker BridgeMaker 생성
    bridgeMaker를 생성하였다.

--- src/main/java/bridge/BridgeMaker.java
+++ src/main/java/bridge/BridgeMaker.java
@@ -1,5 +1,6 @@
 package bridge;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -8,6 +9,10 @@ import java.util.List;
 public class BridgeMaker {
 
     private final BridgeNumberGenerator bridgeNumberGenerator;
+    private String Up = "U";
+    private String Down = "D";
+    private int UpNumber = 1;
+    private int DownNumber = 0;
 
     public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
         this.bridgeNumberGenerator = bridgeNumberGenerator;
@@ -18,6 +23,23 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
+        List<String> answerBridge = new ArrayList<>();
+        for(int i=0;i<size;i++){
+            String moveDirection = checkUpOrDown();
+            answerBridge.add(moveDirection);
+        }
+
+        return answerBridge;
+    }
+
+    private String checkUpOrDown(){
+        int number = bridgeNumberGenerator.generate();
+        if(number == UpNumber){
+            return Up;
+        }
+        if(number == DownNumber){
+            return Down;
+        }
         return null;
     }
 }
